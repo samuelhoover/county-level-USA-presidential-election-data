2c10f59632ddc76201db8f121b0e4882dda25e03
samuelhoover <hooversamuelc@gmail.com>
2023-06-14 21:08:10 -0400

    Added third party and write-in candidate vote share, made dashboard more interactive

--- dashboard.py
+++ dashboard.py
@@ -29,72 +29,79 @@ def fig_to_uri(in_fig, close_all=True, **save_args):
     return f'data:image/png;base64,{encoded}'
 
 
+# read in JSON file
 with open('counties.json') as f:
     counties = json.load(f)
 f.close()
+
+# sort county and state names alphabetically
+counties = sorted(sorted(counties, key=lambda x: x['name']), key=lambda x: x['state'])
+
+# create subset of US presidential election data
 data = [{'county': x['name'], 'state': x['state'], 'year': k} | v for x in counties if 'elections' in x for k, v in x['elections'].items()]
-data = [x | {'dem_share': x['dem'] / x['total']} | {'gop_share': x['gop'] / x['total']} for x in data]
-dem2008 = [x['dem'] / x['total'] for x in data if x['year'] == '2008']
-gop2008 = [x['gop'] / x['total'] for x in data if x['year'] == '2008']
-dem2016 = [x['dem'] / x['total'] for x in data if x['year'] == '2016']
-gop2016 = [x['gop'] / x['total'] for x in data if x['year'] == '2016']
+# calculate vote share by party in each county for each presidential election and save as key-value pairs
+data = [x 
+        | {'dem_share': np.round(x['dem'] / x['total'], 3)} 
+        | {'gop_share': np.round(x['gop'] / x['total'], 3)} 
+        | {'other_share': np.round((x['total'] - x['dem'] - x['gop']) / x['total'], 3)} for x in data]
 
-# fig, ax = plt.subplots()
-# ax.hist(dem2008, color='blue', label='Dem 2008')
-# ax.hist(gop2008, color='red', label='GOP 2008')
-# ax.hist(dem2016, color='blue', label='Dem 2016')
-# ax.hist(gop2016, color='red', label='GOP 2016')
-# ax.set_xlabel('Vote share')
-# ax.set_ylabel('Frequency')
-# ax.legend()
-# fig.tight_layout()
-# fig_to_uri(fig)
-
+# import CSS style sheet
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = Dash(__name__, external_stylesheets=external_stylesheets)
 
+# dashboard
 app.layout = html.Div([
     html.Div(className='row', children='USA Presidential election data',
              style={'textalign': 'left', 'fontSize': 30}
     ),
     
     html.Div(className='row', children=[
-        dcc.RadioItems(options=['2008', '2016', 'both'], value='both', inline=True, id='year-buttons')
+        dcc.Dropdown(
+            id='year-dropdown', 
+            options=['2008', '2012', '2016', '2020'], 
+            multi=True, 
+            placeholder='Select year(s)', 
+            clearable=True
+        )
     ]),
 
     html.Div(className='row', children=[
         html.Div(className='six columns', children=[
-            dash_table.DataTable(data=data, page_size=20),
+            dash_table.DataTable(data=data, page_size=30),
         ]),
         html.Div(className='six columns', children=[
-            dcc.Graph(figure={}, id='vote-share-chart')
+            dcc.Graph(id='vote-share-chart', figure={})
         ])
     ])
 ])
 
 @callback(
     Output(component_id='vote-share-chart', component_property='figure'),
-    Input(component_id='year-buttons', component_property='value')
+    Input(component_id='year-dropdown', component_property='value')
 )
 def update_graph(year):
     fig = go.Figure()
-    if year == '2008':
-        fig.add_trace(go.Histogram(x=dem2008, name='Obama, 2008'))
-        fig.add_trace(go.Histogram(x=gop2008, name='McCain, 2008'))
-    elif year == '2016':
-        fig.add_trace(go.Histogram(x=dem2016, name='Clinton, 2016'))
-        fig.add_trace(go.Histogram(x=gop2016, name='Trump, 2016'))
-    else:
-        fig.add_trace(go.Histogram(x=dem2008, name='Obama, 2008'))
-        fig.add_trace(go.Histogram(x=gop2008, name='McCain, 2008'))
-        fig.add_trace(go.Histogram(x=dem2016, name='Clinton, 2016'))
-        fig.add_trace(go.Histogram(x=gop2016, name='Trump, 2016'))
     fig.update_layout(
+        xaxis_range=[0, 1],
         xaxis_title_text='Share of vote by party in county',
         yaxis_title_text='Frequency',
-        barmode='overlay')
+        barmode='overlay'
+    )
+    if year is None:
+        return fig
+    else:
+        for y in year:
+            fig.add_trace(go.Histogram(
+                x=[x['dem_share'] for x in data if x['year'] == y], name=f'Dem, {y}'
+            ))
+            fig.add_trace(go.Histogram(
+                x=[x['gop_share'] for x in data if x['year'] == y], name=f'GOP, {y}'
+            ))
     fig.update_traces(opacity=0.6)
     return fig
+
+# TODO:
+# add map for county vote share
 
 if __name__ == '__main__':
     app.run_server(debug=True)
